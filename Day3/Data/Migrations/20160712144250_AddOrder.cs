06889f4f9738edcfe9a4371faec7b92bbafce71d
VadzimHuryn <elaineessea@gmail.com>
2016-07-12 21:15:12 +0300

    Day3 Homework

---
+++ Day3/Data/Migrations/20160712144250_AddOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Day3Homework.Data.Migrations
+{
+    public partial class AddOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
